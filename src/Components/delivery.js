5e482a07d2503bf1283c138ea218a7dc689fba91
martinmazas <mazasmartin@gmail.com>
2021-01-12 18:32:50 +0200

    preparing the data

---
+++ src/Components/delivery.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+
+class Delivery extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editing: false
+        }
+
+        this.edit = this.edit.bind(this);
+        this.delete = this.delete.bind(this);
+        this.renderUI = this.renderUI.bind(this);
+        this.renderForm = this.renderForm.bind(this);
+        this.save = this.save.bind(this);
+    }
+
+    edit() {
+        this.setState({
+            editing: true
+        })
+    }
+
+    renderForm() {
+        return(
+            <div>
+                <form>
+                    <textarea ref={input => {this.newDelivery = input}} />
+                    <button onClick={this.save}>Save</button>
+                </form>
+            </div>
+        )
+    }
+
+    save(e) {
+        e.preventDefault();
+
+        this.props.onChange(this.newDelivery.value, this.props.index);
+
+        this.setState({
+            editing: false
+        })
+    }
+
+    delete() {
+        this.props.onDelete(this.props.index);
+    }
+
+    renderUI() {
+        return(
+            <div className="delivery">
+                <div>{this.props.children}</div>
+                <span>
+                    <button onClick={this.edit}>Edit</button>
+                    <button onClick={this.delete}>Delete</button>
+                </span>
+            </div>
+        )
+    }
+
+    render() {
+        return this.state.editing ? this.renderForm() : this.renderUI();
+    }
+}
+
+export default Delivery;
---
+++ src/Components/deliveryInfo.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import Delivery from './delivery';
+import personsData from '../Data/persons.json';
+
+class DeliveryInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deliveries : []
+        }
+        this.eachDelivery = this.eachDelivery.bind(this);
+        this.update = this.update.bind(this);
+        this.delete = this.delete.bind(this);
+        this.add = this.add.bind(this);
+        this.nextId = this.nextId.bind(this);
+    }
+
+    componentDidMount() {
+        personsData.map(item => this.add({id: item.id, date: item.date, name: item.name, city: item.city}));
+    }
+
+    update(newDelivery, i) {
+        this.setState(prevState => ({
+            deliveries: prevState.deliveries.map(
+                delivery => delivery.id !== i ? delivery: {...delivery, delivery: newDelivery})
+        }));
+    }
+
+    delete(id) {
+        this.setState(prevState => ({
+            deliveries: prevState.deliveries.filter(delivery => delivery.id !== id)
+        }))
+    }
+
+    add({id = null, date = null, name = null, city = null}) {
+        this.setState(prevState => ({
+            deliveries: [
+                ...prevState.deliveries, {
+                    id: id !== null ? id:this.nextId(prevState.deliveries),
+                    name: name,
+                    date: date,
+                    city: city
+                }
+            ]
+        }))
+    }
+
+    nextId(deliveries = []) {
+        let max = deliveries.reduce
+        ((prev, curr) => prev.id > curr.id ? prev.id: curr.id, 0)
+        return ++max;
+    }
+
+    eachDelivery(item, i) {
+        return(
+            <div key={i} className="card">
+                <div className="card-body">
+                    <Delivery index={item.id} onChange={this.update} onDelete={this.delete}>
+                        <h4>{item.name}</h4>
+                        <h4>{item.date}</h4>
+                        <h4>{item.city}</h4>
+                    </Delivery>
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        return(
+            <div className='deliveries-list'>
+                {this.state.deliveries.map(this.eachDelivery)}
+                <button onClick={this.add}>Add</button>
+            </div>
+        )
+    }
+}
+
+export default DeliveryInfo;
